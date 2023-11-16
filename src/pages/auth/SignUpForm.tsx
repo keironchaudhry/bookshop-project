020d656b306311ccc75dd4c517f056e17f7a3966
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-11-16 09:45:54 +0100

    Feat: Create signup page scaffolding

--- src/pages/auth/SignUpForm.tsx
+++ src/pages/auth/SignUpForm.tsx
@@ -1,6 +1,29 @@
+import { Link } from "react-router-dom";
+
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export function SignUpForm() {
   return (
-    <div>SignUpForm</div>
-  )
+    <Container>
+      <h2>Sign Up</h2>
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="text" placeholder="Enter your email address" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Enter password</Form.Label>
+          <Form.Control type="text" placeholder="Enter a password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Confirm password</Form.Label>
+          <Form.Control type="text" placeholder="Confirm your password" />
+        </Form.Group>
+        <Button as="input" type="submit" value="Submit" />
+      </Form>
+      <p>Already have an account? <Link to="/login">Login here</Link></p>
+    </Container>
+  );
 }
