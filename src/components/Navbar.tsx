2507e605bd9cd78d2b0d9beb0d7d5a9024cb8a95
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-11-16 10:58:13 +0100

    Feat: Add sign up to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,8 +18,11 @@ const Navbar: React.FC = () => {
           <Nav.Link as={Link} to="/">
             Home
           </Nav.Link>
-          <Nav.Link as={Link} to="/signin">
-            Sign In
+          <Nav.Link as={Link} to="/login">
+            Login
+          </Nav.Link>
+          <Nav.Link as={Link} to="/signup">
+            Sign Up
           </Nav.Link>
           <Nav.Link as={Link} to="/shopping">
             <FontAwesomeIcon icon={faCartShopping} />
