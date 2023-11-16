ca641fc452744484e8105827af56c67c3c32f8f2
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-11-16 09:58:42 +0100

    Feat: Create logic for user data collection

--- src/pages/auth/SignUpForm.tsx
+++ src/pages/auth/SignUpForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { Link } from "react-router-dom";
 
 import Container from "react-bootstrap/Container";
@@ -5,25 +7,58 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 export function SignUpForm() {
+  const [signUpData, setSignUpData] = useState({
+    email: "",
+    password1: "",
+    password2: "",
+  });
+
+  const handleChange = (event: { target: { name: any; value: any } }) => {
+    setSignUpData({
+      ...signUpData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <Container>
       <h2>Sign Up</h2>
       <Form>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="text" placeholder="Enter your email address" />
+          <Form.Control
+            type="text"
+            placeholder="Enter your email address"
+            name="email"
+            value={signUpData.email}
+            onChange={handleChange}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <Form.Label>Enter password</Form.Label>
-          <Form.Control type="text" placeholder="Enter a password" />
+          <Form.Control
+            type="password"
+            placeholder="Enter a password"
+            name="password1"
+            value={signUpData.password1}
+            onChange={handleChange}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <Form.Label>Confirm password</Form.Label>
-          <Form.Control type="text" placeholder="Confirm your password" />
+          <Form.Control
+            type="password"
+            placeholder="Confirm your password"
+            name="password2"
+            value={signUpData.password2}
+            onChange={handleChange}
+          />
         </Form.Group>
         <Button as="input" type="submit" value="Submit" />
       </Form>
-      <p>Already have an account? <Link to="/login">Login here</Link></p>
+      <p>
+        Already have an account? <Link to="/login">Login here</Link>
+      </p>
     </Container>
   );
 }
