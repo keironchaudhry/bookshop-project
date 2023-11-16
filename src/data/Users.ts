3c425bbc6e577a73703777e774aaa8fcda58b87e
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-11-16 10:56:36 +0100

    Feat: Handle mock user info

--- src/data/Users.ts
+++ src/data/Users.ts
@@ -4,12 +4,18 @@ export interface User {
   id: number;
   name: string;
   email: string;
+  password: string;
 }
 
-const mockUsers: User[] = [
-  { id: 1, name: "Jody Murray", email: "jody@example.com" },
-  { id: 2, name: "Keiron Chaudhry", email: "keiron@example.com" },
-  { id: 3, name: "Yoda Murray", email: "yoda@example.com" },
+let mockUsers: User[] = [
+  { id: 1, name: "Jody Murray", email: "jody@example.com", password: "123" },
+  { id: 2, name: "Keiron Chaudhry", email: "keiron@example.com", password: "123" },
+  { id: 3, name: "Yoda Murray", email: "yoda@example.com", password: "123" },
 ];
+
+export const addMockUser = (newUser: User): void => {
+  newUser.id = mockUsers.length + 1;
+  mockUsers.push(newUser);
+};
 
 export default mockUsers;
--- src/pages/auth/SignUpForm.tsx
+++ src/pages/auth/SignUpForm.tsx
@@ -1,29 +1,78 @@
 import { useState } from "react";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
+import mockUsers, { addMockUser, User } from "../../data/Users";
+
 export function SignUpForm() {
   const [signUpData, setSignUpData] = useState({
+    name: "",
     email: "",
     password1: "",
     password2: "",
   });
 
-  const handleChange = (event: { target: { name: any; value: any } }) => {
+  const handleChange = (event: { target: { name: string; value: string } }) => {
     setSignUpData({
       ...signUpData,
       [event.target.name]: event.target.value,
     });
   };
 
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    try {
+      if (signUpData.password1 !== signUpData.password2) {
+        alert("Passwords do not match");
+        return;
+      } else {
+        console.log("Passwords match");
+      }
+
+      const newUser: User = {
+        id: 0,
+        name: signUpData.name,
+        email: signUpData.email,
+        password: signUpData.password1,
+      };
+
+      addMockUser(newUser);
+      console.log("New user added: ", newUser);
+      console.log(mockUsers);
+
+      setSignUpData({
+        name: "",
+        email: "",
+        password1: "",
+        password2: "",
+      });
+
+      navigate("/login");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Container>
       <h2>Sign Up</h2>
-      <Form>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Full name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Your full name"
+            name="name"
+            value={signUpData.name}
+            onChange={handleChange}
+          />
+        </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>Email address</Form.Label>
           <Form.Control
@@ -54,7 +103,9 @@ export function SignUpForm() {
             onChange={handleChange}
           />
         </Form.Group>
-        <Button as="input" type="submit" value="Submit" />
+        <Button type="submit" variant="primary">
+          Create account
+        </Button>
       </Form>
       <p>
         Already have an account? <Link to="/login">Login here</Link>
