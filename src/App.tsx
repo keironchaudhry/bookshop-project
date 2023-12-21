cb2e30f9d0df6f438b54bdd496613604927c232a
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-12-21 16:39:31 +0100

    Feat: Create route to basket

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { SignUpForm } from "./pages/auth/SignUpForm";
 import { LoginForm } from "./pages/auth/LoginForm";
 import { CurrentUserProvider } from "./context/CurrentUserContext";
 import DetailView from "./pages/shop/DetailView";
+import ShoppingBasket from "./pages/shop/ShoppingBasket";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/signup" element={<SignUpForm />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/shop/:id" element={<DetailView />} />
+          <Route path="/basket" element={<ShoppingBasket />} />
         </Routes>
       </CurrentUserProvider>
     </div>
